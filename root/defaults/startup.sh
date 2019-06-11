8c95509322c93b99d93e2880112b82eb47594e50
Gaston Funes <gtfunes@gmail.com>
2019-06-11 20:54:07 -0300

    Install mdns service in package json

--- root/defaults/startup.sh
+++ root/defaults/startup.sh
@@ -19,6 +19,8 @@
 # If you're running a debian based version of this container please use apt-get to install packages.
 #
 
+npm --no-update-notifier install --unsafe-perm --save mdns homebridge-camera-ffmpeg homebridge-hue homebridge-people homebridge-plex-v2 homebridge-samsung-tizen homebridge-winpc
+
 apk add --no-cache ffmpeg ffmpeg-libs iputils
 
 usermod -aG video $(whoami)
